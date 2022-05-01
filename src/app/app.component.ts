7c81489cfac9c2da322d27fd5491bf00f8461969
Zoyafng <zhouyafeng@authing.cn>
2022-05-02 01:03:24 +0800

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
-  title = 'ng-guard';
+  title = 'Authing';
+
+  appId = '5d70d0e991fdd597019df70d';
+
+  visible = true;
+
+  onLoad() {
+    console.log(this.visible, '加载了');
+  }
+
+  onClose() {
+    this.visible = false;
+
+    console.log('关闭了');
+  }
+  config: any = {
+    mode: 'modal',
+  };
+
+  onLogin(event: any) {
+    console.log('登录了：', event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GuardModule, AuthingGuardModule } from '@authing/ng-ui-components';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, GuardModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
